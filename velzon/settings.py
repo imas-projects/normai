8a464347f4e271f8b00a0bd4f745ed66f1647f7d
reyesrr <72855373+reyesrr@users.noreply.github.com>
2025-01-21 11:24:08 +0100

    Update settings.py
    cambios en allowed hosts

--- velzon/settings.py
+++ velzon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j%^*y0krq5^-#3lggoecxw!d7ad_gqkab3t5w17&0w06+qf8+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.25.185.243','127.0.0.1']
+ALLOWED_HOSTS = ['34.254.185.243','127.0.0.1','localhost']
 
 
 # Application definition
