c9582092082b85683aaed4533c408aa8c3d79c5b
alexblancobermejo <alexblancobermejo@gmail.com>
2025-03-17 11:30:30 +0100

    Update urls.py

--- risks/urls.py
+++ risks/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from .views import create_risk
-from .views import generate_risks_pdf, get_risk_data, update_risk
+from .views import generate_risks_pdf, get_risk_data
 from . import views
 
 app_name = 'risks'
 
 urlpatterns = [
     path("get-risk-data/<int:risk_id>/", get_risk_data, name="get-risk-data"),
-    path("update-risk/<int:risk_id>/step-<int:step>/", update_risk, name="update-risk-step"),
     path('', views.create_risk, name='risks'),
     path('download-risks-pdf/<str:department_name>/', generate_risks_pdf, name='download_risks_pdf'),
     path("save-risk-step/<int:step>/", views.save_risk_step, name="save-risk-step")
