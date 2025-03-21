a855fe2e0523f524b102995025a153d966f7fc95
alexblancobermejo <alexblancobermejo@gmail.com>
2025-03-21 14:13:41 +0100

    Update urls.py

--- risks/urls.py
+++ risks/urls.py
@@ -6,8 +6,16 @@ from . import views
 app_name = 'risks'
 
 urlpatterns = [
-    path("get-risk-data/<int:risk_id>/", get_risk_data, name="get-risk-data"),
-    path('', views.create_risk, name='risks'),
     path('download-risks-pdf/<str:department_name>/', generate_risks_pdf, name='download_risks_pdf'),
-    path("save-risk-step/<int:step>/", views.save_risk_step, name="save-risk-step")
+    path('', create_risk, name='risks'),
+    path('add-risk-identification/', add_risk_identification, name='add_risk_identification'),
+    path('add-risk-evaluation/', add_risk_evaluation, name='add_risk_evaluation'),
+    path('add-risk-treatment/', add_risk_treatment, name='add_risk_treatment'),
+    path('add-contingency-plan/', add_contingency_plan, name='add_contingency_plan'),
+    path('add-reevaluation/', add_reevaluation, name='add_reevaluation'),
+    path('edit-risk-identification/<int:risk_id>/', edit_risk_identification, name='edit-risk-identification'),
+    path('edit-risk-evaluation/<int:risk_id>/', edit_risk_evaluation, name='edit-risk-evaluation'),
+    path('edit-risk-treatment/<int:risk_id>/', edit_risk_treatment, name='edit-risk-treatment'),
+    path('edit-contingency-plan/<int:risk_id>/', edit_contingency_plan, name='edit-contingency-plan'),
+    path('edit-reevaluation/<int:risk_id>/', edit_reevaluation, name='edit-reevaluation'),
   ]
