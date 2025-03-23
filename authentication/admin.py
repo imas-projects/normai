72f302e33ca6336af2c0b707d3eccea7e919cc6e
reyesrr <MReyes.Rodriguez@alu.uclm.es>
2025-03-23 16:47:22 +0100

    update

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Permission
 
 # Register your models here.
-# 'auth.GroupAdmin'
-# 'auth.UserAdmin'
+admin.site.register(Permission)
