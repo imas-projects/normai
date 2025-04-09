76a9e56509bd1f9aa295286dbc1af0c4af47bcb9
alexblancobermejo <alexblancobermejo@gmail.com>
2025-04-09 16:33:08 +0200

    Update admin.py

--- risks/admin.py
+++ risks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Department)
+admin.site.register(models.Role)
+admin.site.register(models.ContingencyAction)
+admin.site.register(models.RiskLevel)
+admin.site.register(models.RiskIdentification)
+admin.site.register(models.RiskEvaluation)
+admin.site.register(models.RiskTreatment)
+admin.site.register(models.ContingencyPlan)
+admin.site.register(models.Reevaluation)
