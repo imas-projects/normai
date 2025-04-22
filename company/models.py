00f0101392c77399573746a15fde8254cb62f8a6
alexblancobermejo <alexblancobermejo@gmail.com>
2025-04-22 18:32:02 +0200

    Update models.py

--- company/models.py
+++ company/models.py
@@ -46,17 +46,59 @@ class ExternalClient(models.Model):
     
 #Areas
 class Area(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Area Name")
-    users = models.ManyToManyField(User, related_name="users")  # Un área puede tener varios usuarios y cada usuario estar en más de un area
-    
+    name = models.CharField(max_length=200, verbose_name="Area Name or Position Title")
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="sub_areas",
+        verbose_name="Parent Area"
+    )
+    users = models.ManyToManyField(User, related_name="areas", blank=True)
+
+    def __str__(self):
+        return f"{self.name}" if not self.parent else f"{self.name} (under {self.parent.name})"
+
     class Meta:
         db_table = 'tb_company_area'
-    
-    def __str__(self):
-        return self.name
-    
+
     def as_dict(self):
-        return {"id": self.id, "name": self.name}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "parent": self.parent.as_dict() if self.parent else None,
+            "users": [{
+                "id": u.id,
+                "username": u.username,
+                "first_name": u.first_name,
+                "last_name": u.last_name,
+                "email": u.email
+            } for u in self.users.all()],
+        }
+
+class Requirement(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Requirement Name")
+    parent = models.ForeignKey(
+        'self', 
+        on_delete=models.PROTECT, 
+        null=True, 
+        blank=True, 
+        related_name="sub_requirements"
+    )
+
+    def __str__(self):
+        return f"{self.name}" if not self.parent else f"{self.name} (under {self.parent.name})"
+
+    class Meta:
+        db_table = 'tb_company_requirement'
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "parent": self.parent.as_dict() if self.parent else None,
+        }
 
 #Roles
 class Rol(models.Model):
