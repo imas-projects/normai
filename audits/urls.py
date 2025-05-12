55be0d45d50dc4be632be4cc7f59f679acec8c72
alexblancobermejo <alexblancobermejo@gmail.com>
2025-05-12 09:44:49 +0200

    Update urls.py

--- audits/urls.py
+++ audits/urls.py
@@ -1,17 +1,20 @@
-# audits/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'audits'
 
 urlpatterns = [
+    # Página principal de auditorías
     path('audits/', views.audits_home, name='audits_home'),
-    path('annual-audit-program/', views.annual_audit_program, name='annual_program'), 
-    path('annual-audit-plan/', views.annual_audit_plan, name='annual_plan'),  
-    path('conduct-internal-audits/', views.conduct_internal_audits, name='conduct_audit'),
+
+    # Páginas de los programas de auditoría
+    path('annual-audit-program/', views.annual_audit_program, name='annual_audit_program'), 
+    path('annual-audit-plan/', views.annual_audit_plan, name='annual_audit_plan'),  
+    path('conduct-internal-audits/', views.conduct_internal_audits, name='conduct_internal_audits'),  
 
     # Formularios para agregar datos
     path('add-audited/', views.add_audited, name='add_audited'),
+    path('add-checklist-question/', views.add_checklist_question, name='add_checklist_question'), 
     path('add-audited-evaluation-question/', views.add_audited_evaluation_question, name='add_audited_evaluation_question'),
     path('add-lead-auditor-evaluation-question/', views.add_lead_auditor_evaluation_question, name='add_lead_auditor_evaluation_question'),
     path('add-audit-program-header/', views.add_audit_program_header, name='add_audit_program_header'),
@@ -21,11 +24,9 @@ urlpatterns = [
     path('add-findings/', views.add_findings, name='add_findings'),
     path('add-audit-report/', views.add_audit_report, name='add_audit_report'),
     path('add-requirement/', views.add_requirement, name='add_requirement'),
-    path('add-checklist/', views.add_checklist, name='add_checklist'),
 
     # Endpoints para obtener datos dinámicamente (AJAX)
-    path('get_checklist_data/<int:requirement_id>/', views.get_checklist_data, name='get_checklist_data'),
+    path('get-checklist-data/<int:requirement_id>/', views.get_checklist_data, name='get_checklist_data'), 
     path('get-audited-questions/<int:requirement_id>/', views.get_audited_questions, name='get_audited_questions'),
     path('get-lead-auditor-questions/<int:requirement_id>/', views.get_lead_auditor_questions, name='get_lead_auditor_questions'),
 ]
-
