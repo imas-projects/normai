3b90e79fdc7ad65f69df1174d47c0d9e5de1f178
alexblancobermejo <alexblancobermejo@gmail.com>
2025-06-16 18:57:11 +0200

    Update urls.py

--- risks/urls.py
+++ risks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import create_risk, edit_risk_identification, edit_risk_evaluation, edit_risk_treatment, edit_contingency_plan, edit_reevaluation
-from .views import generate_risks_pdf, add_risk_identification, add_risk_evaluation, add_risk_treatment, add_contingency_plan, add_reevaluation, get_evaluation_suggestions, get_treatment_suggestions
+from .views import generate_risks_pdf, add_risk_identification, add_risk_evaluation, add_risk_treatment, add_contingency_plan, add_reevaluation, get_controls_suggestions, get_ranges_suggestions, get_level_suggestions, get_treatment_suggestions
 from . import views
 
 
@@ -14,7 +14,9 @@ urlpatterns = [
   path('add-risk-evaluation/', add_risk_evaluation, name='add_risk_evaluation'),
   path('get-evaluation-suggestions/', get_evaluation_suggestions, name='get_evaluation_suggestions'),
   path('add-risk-treatment/', add_risk_treatment, name='add_risk_treatment'),
-  path('get-treatment-suggestions/', get_treatment_suggestions, name='get_treatment_suggestions'),
+  path('get-controls-suggestions/', get_controls_suggestions, name='get_controls_suggestions'),
+  path('get-ranges-suggestions/', get_ranges_suggestions, name='get_ranges_suggestions'),
+  path('get-level-suggestions/', get_level_suggestions, name='get_level_suggestions'),
   path('add-contingency-plan/', add_contingency_plan, name='add_contingency_plan'),
   path('add-reevaluation/', add_reevaluation, name='add_reevaluation'),
   path('edit-risk-identification/<int:risk_id>/', edit_risk_identification, name='edit-risk-identification'),
