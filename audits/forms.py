b6c9dc624a9917495ef1f023418799b6e3405c4a
alexblancobermejo <alexblancobermejo@gmail.com>
2025-06-24 18:30:13 +0200

    Update forms.py

--- audits/forms.py
+++ audits/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import (
-    AuditProgramHeader, AnnualProgram, AnnualPlan, AnnualProgramUser,
+    AuditProgramHeader, AnnualProgram, AnnualPlan,
     AnnualPlanAuditor, AnnualPlanAudited, Checklist, Findings, AuditReport,
     ProcessRequirement, AuditedEvaluationQuestion, AuditorEvaluation, LeadAuditorEvaluationQuestion
 )
@@ -26,15 +26,6 @@ class AnnualProgramForm(forms.ModelForm):
             'program_header': forms.Select(attrs={'class': 'form-control'}),
             'process': forms.Select(attrs={'class': 'form-control'}),
             'month': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 12, 'placeholder': 'Month (1-12)'}),
-        }
-
-class AnnualProgramUserForm(forms.ModelForm):
-    class Meta:
-        model = AnnualProgramUser
-        fields = ['annual_program', 'user']
-        widgets = {
-            'annual_program': forms.Select(attrs={'class': 'form-control'}),
-            'user': forms.Select(attrs={'class': 'form-control'}),
         }
 
 class AnnualPlanForm(forms.ModelForm):
@@ -88,13 +79,14 @@ class ChecklistForm(forms.ModelForm):
 class FindingsForm(forms.ModelForm):
     class Meta:
         model = Findings
-        fields = ['report', 'requirement', 'finding_text', 'classification']
+        fields = ['audit_plan', 'requirement', 'finding_text', 'classification']
         widgets = {
-            'report': forms.Select(attrs={'class': 'form-control'}),
+            'audit_plan': forms.Select(attrs={'class': 'form-control'}),
             'requirement': forms.Select(attrs={'class': 'form-control'}),
             'finding_text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'classification': forms.Select(attrs={'class': 'form-control'}),
         }
+
 
 class AuditReportForm(forms.ModelForm):
     class Meta:
@@ -127,12 +119,12 @@ class AuditedEvaluationQuestionForm(forms.ModelForm):
 class AuditorEvaluationForm(forms.ModelForm):
     class Meta:
         model = AuditorEvaluation
-        fields = ['audit', 'question', 'orden', 'rate']
+        fields = ['audit_plan', 'question', 'orden', 'rate']
         widgets = {
-            'audit': forms.Select(attrs={'class': 'form-control'}),
+            'audit_plan': forms.Select(attrs={'class': 'form-control'}),
             'question': forms.Select(attrs={'class': 'form-control'}),
             'orden': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
-            'rate': forms.NumberInput(attrs={'class': 'form-control', 'min': 0, 'max': 10}),  # Ajusta el rango si es necesario
+            'rate': forms.NumberInput(attrs={'class': 'form-control', 'min': 0, 'max': 10}),
         }
 
 class LeadAuditorEvaluationQuestionForm(forms.ModelForm):
