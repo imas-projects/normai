cd93c6c5718afaec6bd65aeee82f740697bca39e
alexblancobermejo <alexblancobermejo@gmail.com>
2025-06-25 18:56:34 +0200

    Update urls.py

--- audits/urls.py
+++ audits/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('add_annual_plan/', views.add_annual_plan, name='add_annual_plan'),
     path('add_annual_plan_auditor/', views.add_annual_plan_auditor, name='add_annual_plan_auditor'),
     path('suggest-auditor/', views.suggest_auditor_view, name='suggest_auditor'),
+    path('save-selected-auditor/', views.save_selected_auditor, name='save_selected_auditor'),
     path('add_annual_plan_audited/', views.add_annual_plan_audited, name='add_annual_plan_audited'),
     path('add_checklist/', views.add_checklist, name='add_checklist'),
     path('add_findings/', views.add_findings, name='add_findings'),
