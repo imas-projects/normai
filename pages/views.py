698228c03ad63bcf0f0124e87f90a58529a3b897
alexblancobermejo <alexblancobermejo@gmail.com>
2025-07-01 19:15:53 +0200

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -110,9 +110,8 @@ def wellcome_view(request):
     areas = Area.objects.all()
 
     processes_with_findings = Process.objects.annotate(
-        total_findings=Count('processrequirement__findings_set')
+        total_findings=Count('processrequirement__findings')
     ).order_by('-total_findings')
-
 
 
     # === Contexto final ===
