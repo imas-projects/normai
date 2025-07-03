9ff696fb0a8ffafa3e521ad4f3a1f940bc5653b1
alexblancobermejo <alexblancobermejo@gmail.com>
2025-07-03 12:48:51 +0200

    Update settings.py

--- velzon/settings.py
+++ velzon/settings.py
@@ -97,6 +97,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pages.context_processors.areas',
+
             ],
         },
     },
