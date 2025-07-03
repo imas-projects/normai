b42b70ce97fea6f3c19cc450f2300617ece65c8c
alexblancobermejo <alexblancobermejo@gmail.com>
2025-07-03 12:55:42 +0200

    Update context_processors.py

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -1,6 +1,8 @@
 from company.models import Area
 
 def areas(request):
+    areas_qs = Area.objects.all()
+    print("Context processor areas count:", areas_qs.count()) 
     return {
-        'all_areas': Area.objects.all()
+        'all_areas': areas_qs
     }
