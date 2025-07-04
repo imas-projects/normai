a42f1d7a462124da9e21ce91f62a8fb84808969e
alexblancobermejo <alexblancobermejo@gmail.com>
2025-07-04 23:02:41 +0200

    Update views.py

--- audits/views.py
+++ audits/views.py
@@ -323,11 +323,11 @@ def conduct_internal_audits(request):
         audit_report__audit_plan__findings__classification__in=['NC_MAYOR', 'NC_MENOR', 'OPORTUNIDAD_MEJORA']
     ).distinct()
 
-    for action in acciones:
-        findings = action.audit_report.audit_plan.findings.all()
-        if findings.exists():
-            sev_num = severity_map.get(findings.first().classification, 0)
-            duracion_dias = (now().date() - action.due_date).days if action.due_date else 0
+    if action.due_date:
+        duracion_dias = (now().date() - action.due_date).days
+        duracion_dias = duracion_dias if duracion_dias > 0 else 0
+    else:
+        duracion_dias = 0
 
             scatter_data.append({
                 'x': duracion_dias,
